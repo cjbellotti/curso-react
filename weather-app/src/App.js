fa27a0c208b588f5cff6fc0bfe59196fadb64ce0
Carlos Bellotti <cjbellotti@hotmail.com>
2019-07-01 00:31:29 -0300

    Commit inicial

---
+++ weather-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherLocation from './components/WeatherLocation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Weather app!!!
+        <WeatherLocation/>
+    </div>
+  );
+}
+
+export default App;
